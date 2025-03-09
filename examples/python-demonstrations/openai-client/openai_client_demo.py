19c3e5a09327399f2885877a1950698495e5ddcd
EvgenySivaykin <114056472+EvgenySivaykin@users.noreply.github.com>
2025-03-09 18:26:01 +0200

    commit 2

---
+++ examples/python-demonstrations/openai-client/openai_client_demo.py
@@ -0,0 +1,10 @@
+# api_key = 'sk-1234567890abcdef1234567890abcdef'
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv('OPENAI_API_KEY')
+
+client = OpenAIClient(api_key)
